bb31d69fc8fe60de46504e75296aa07085528faa
GaneshShihire <shihireganesh0@gmail.com>
2026-01-17 22:39:24 +0530

    Added classes update and delete student

--- src/dao/StudentDAO.java
+++ src/dao/StudentDAO.java
@@ -2,37 +2,113 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import model.Student;
 import util.DBConnection;
 
-
 public class StudentDAO {
-    
-    public boolean addStudent(Student student){
+
+    public boolean addStudent(Student student) {
         boolean isInserted = false;
 
         String sql = "insert into students (name, email, course) values (?, ?, ?)";
 
-        try{
+        try {
             Connection con = DBConnection.getConnection();
             PreparedStatement ps = con.prepareStatement(sql);
 
-            ps.setString(1,  student.getName());
-            ps.setString(2,  student.getEmail());
-            ps.setString(3,  student.getCourse());
+            ps.setString(1, student.getName());
+            ps.setString(2, student.getEmail());
+            ps.setString(3, student.getCourse());
 
             int rows = ps.executeUpdate();
 
-            if(rows > 0){
+            if (rows > 0) {
                 isInserted = true;
             }
 
-
-        }catch(Exception e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
         return isInserted;
     }
+
+    public void getAllStudents() {
+
+        String query = "SELECT * FROM students";
+
+        try {
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement ps = conn.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+
+            System.out.println("\nID | NAME | EMAIL | COURSE");
+            System.out.println("-----------------------------------");
+
+            while (rs.next()) {
+
+                int id = rs.getInt("student_id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String course = rs.getString("course");
+
+                System.out.println(id + " | " + name + " | " + email + " | " + course);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateStudent(int student_id, String name, String email, String course) {
+
+        try {
+            Connection conn = DBConnection.getConnection();
+
+            String query = "update students set name=?, email=?, course=? where student_id=?";
+            PreparedStatement ps = conn.prepareStatement(query);
+
+            ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setString(3, course);
+            ps.setInt(4, student_id);
+
+            int rows = ps.executeUpdate();
+
+            if (rows > 0) {
+                System.out.println("Updated Successfully");
+            } else {
+                System.out.println("Student not exists !!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteStudent(int student_id) {
+        try {
+            Connection conn = DBConnection.getConnection();
+
+            String query = "delete from students where student_id=?";
+            PreparedStatement ps = conn.prepareStatement(query);
+
+            ps.setInt(1, student_id);
+
+            int rows = ps.executeUpdate();
+
+            if (rows > 0) {
+                System.out.println("Deleted Successfully");
+            } else {
+                System.out.println("Student not exist");
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/main/SSMSApp.java
+++ src/main/SSMSApp.java
@@ -1,28 +1,81 @@
 package main;
 
-//import java.sql.Connection;
-
+import java.util.Scanner;
 import dao.StudentDAO;
 import model.Student;
-//import util.DBConnection;
 
 public class SSMSApp {
+
     public static void main(String[] args) {
 
-        Student student = new Student(
-
-                "Ganesh",
-                "ganesh@gmail.com",
-                "Java FullStack");
-
+        Scanner sc = new Scanner(System.in);
         StudentDAO dao = new StudentDAO();
-        boolean result = dao.addStudent(student);
 
-        if (result) {
-            System.out.println("Inserted Successfully");
-        } else {
-            System.out.println("Failed to insert");
+        while (true) {
+            System.out.println("\n===== STUDENT MANAGEMENT SYSTEM =====");
+            System.out.println("1. Add Student");
+            System.out.println("2. View All Students");
+            System.out.println("3. Update Student");
+            System.out.println("4. Delete Student");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+
+                    sc.nextLine(); // buffer clear
+                    System.out.print("Enter Student Name: ");
+                    String name = sc.nextLine();
+
+                    System.out.print("Enter Student Email: ");
+                    String email = sc.nextLine();
+
+                    System.out.print("Enter Student Course: ");
+                    String course = sc.nextLine();
+
+                    Student s = new Student(name, email, course);
+                    dao.addStudent(s);
+                    break;
+
+                case 2:
+                    dao.getAllStudents();
+                    break;
+
+                case 3:
+                    System.out.print("Enter Student ID to Update: ");
+                    int uid = sc.nextInt();
+
+                    sc.nextLine();
+                    System.out.print("Enter New Name: ");
+                    String newName = sc.nextLine();
+
+                    System.out.print("Enter New Email: ");
+                    String newEmail = sc.nextLine();
+
+                    System.out.print("Enter New Course: ");
+                    String newCourse = sc.nextLine();
+
+                    dao.updateStudent(uid, newName, newEmail, newCourse);
+                    break;
+
+                case 4:
+                    System.out.print("Enter Student ID to Delete: ");
+                    int did = sc.nextInt();
+
+                    dao.deleteStudent(did);
+                    break;
+
+                case 5:
+                    System.out.println("Thank you! Application Closed.");
+                    sc.close();
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid Choice! Try Again.");
+            }
         }
-
     }
 }
